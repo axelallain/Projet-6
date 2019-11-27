daa365f9fc2ecfff3249b43d55d4bc19d3b96862
Axel Allain <axel.allain.a@gmail.com>
2019-11-27 23:41:42 +0100

    Réactivation du cache Thymeleaf dans application.properties - Génération du .war dans le dossier target - Ajout du PDF de solution technique à la racine du projet

--- src/main/java/fr/axelallain/Main.java
+++ src/main/java/fr/axelallain/Main.java
@@ -2,16 +2,23 @@ package fr.axelallain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableConfigurationProperties
 @ComponentScan("fr.axelallain")
-public class Main {
+public class Main extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Main.class);
+    }
 
 }
